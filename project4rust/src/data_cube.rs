b8c32c64d1fe38312acb93f9d8c2a1fca31595a5
OnikenX <OnikenX@protonmail.com>
2021-07-13 01:31:31 +0000

    start lol

---
+++ project4rust/src/data_cube.rs
@@ -0,0 +1,10 @@
+public struct DataCube{
+    vec<ShellFragment> shellFragmentList;
+    i32 lower;
+}
+
+impl DataCube{
+    fun create(rawData : vec<vec<i32>>, maxValue: vec<vec<i32>>, lowerValue : vec<vec<i32>>) -> DataCube{
+        var shellFragmentList  = 
+    }
+}
